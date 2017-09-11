fc51e3f102fef74e8d31eab41bf72eda5b49e784
ianmoeser <imoeser@cca.edu>
2017-09-10 22:47:36 -0700

    jjj

---
+++ exercise-charrette/assets/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('#button-11').click('click touchstart', function() {
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.8'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.6'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.4'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.2'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.4'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.6'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.8'}, 200);
+		$(this).animate({opacity: '.6'}, 200);
+
+	});
+
+});
